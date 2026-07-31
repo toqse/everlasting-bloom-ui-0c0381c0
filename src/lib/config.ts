0eb69f06bb09de88504312e0b201dfdeeb156a7d
adsl-toqse <adsl@toqse.com>
2026-07-31 12:50:07 +0530

    Updated base url

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 //export const BASE_URL = "http://192.168.1.45:8000/api/";
-export const BASE_URL = "http://127.0.0.1:8000/api/";
+export const BASE_URL = "https://api.aiswaryamatrimonials.com/api/";
