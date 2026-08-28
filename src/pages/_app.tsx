768adf4b096f4c8617473599d9f4812a88c82867
adsl-toqse <adsl@toqse.com>
2026-08-28 17:23:54 +0530

    Build error fixes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import "../../app/globals.css";
 import { plusJakarta, playfair } from "@/lib/fonts";
+import { Providers } from "../../app/providers";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={`${plusJakarta.variable} ${playfair.variable} min-h-screen`}>
-      <Component {...pageProps} />
-    </div>
+    <Providers>
+      <div className={`${plusJakarta.variable} ${playfair.variable} min-h-screen`}>
+        <Component {...pageProps} />
+      </div>
+    </Providers>
   );
 }
